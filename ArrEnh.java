0d15ea9e44a58b5ddf5034de29590e7b02de42d5
swati sinha <swatisinha@gmail.com>
2025-11-12 21:12:28 +0530

    foreach array work

---
+++ ArrEnh.java
@@ -0,0 +1,11 @@
+public class ArrEnh {
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Using enhanced for loop to iterate through the array
+        for (int num : numbers) {
+            System.out.println("Number: " + num);
+        }
+    }
+}
